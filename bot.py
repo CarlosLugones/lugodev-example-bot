f09855ddcda04d914e9ae258417ea2411bf32057
Carlos Lugones <contact@carloslugones.com>
2020-10-11 18:08:29 -0400

    add: Added QR handler

--- bot.py
+++ bot.py
@@ -1,9 +1,59 @@
-from telegram.ext import Updater, CommandHandler
+import os
+import qrcode
+from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
+from telegram import ChatAction
+
+
+INPUT_TEXT = 0
 
 
 def start(update, context):
 
-    update.message.reply_text('Hola, humano!')
+    update.message.reply_text('Hola, bienvenido, qué deseas hacer?\n\nUsa /qr para generar un código qr.')
+
+
+def qr_command_handler(update, context):
+
+    update.message.reply_text('Envíame el texto para generarte un código QR')
+
+    return INPUT_TEXT
+
+
+def generate_qr(text):
+
+    filename = text + '.jpg'
+
+    img = qrcode.make(text)
+    img.save(filename)
+
+    return filename
+
+
+def send_qr(filename, chat):
+
+    chat.send_action(
+        action=ChatAction.UPLOAD_PHOTO,
+        timeout=None
+    )
+
+    chat.send_photo(
+        photo=open(filename, 'rb')
+    )
+
+    os.unlink(filename)
+
+
+def input_text(update, context):
+
+    text = update.message.text
+
+    filename = generate_qr(text)
+
+    chat = update.message.chat
+
+    send_qr(filename, chat)
+
+    return ConversationHandler.END
 
 
 if __name__ == '__main__':
@@ -13,6 +63,18 @@ if __name__ == '__main__':
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start))
+
+    dp.add_handler(ConversationHandler(
+        entry_points=[
+            CommandHandler('qr', qr_command_handler)
+        ],
+
+        states={
+            INPUT_TEXT: [MessageHandler(Filters.text, input_text)]
+        },
+
+        fallbacks=[]
+    ))
 
     updater.start_polling()
     updater.idle()

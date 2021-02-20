76319b960b9a9fe4fe2d1e1e3e4d0b4f4edf2b4a
Carlos Lugones <contact@lugodev.com>
2021-02-20 10:40:12 -0500

    feat: filter messages and send them to private channel if hashtag is present

--- bot.py
+++ bot.py
@@ -1,5 +1,16 @@
 import os
-from telegram.ext import Updater
+from telegram.ext import Updater, MessageHandler, Filters
+
+
+def process_message(update, context):
+
+    text = update.message.text
+
+    if str(text).__contains__('#channel'):
+        context.bot.send_message(
+            chat_id='YOUR_CHANNEL_ID',
+            text=str(text).replace('#channel', '')
+        )
 
 
 if __name__ == '__main__':
@@ -7,5 +18,9 @@ if __name__ == '__main__':
     updater = Updater(token=os.environ['TOKEN'], use_context=True)
 
     dp = updater.dispatcher
+    dp.add_handler(MessageHandler(filters=Filters.text, callback=process_message))
     updater.start_polling()
+
+    print('Bot is polling')
+
     updater.idle()
